259085014092f47a420497d335fd785819f95a7a
Yan Qian <qianyan@thoughtworks.com>
2018-03-17 22:56:41 +0800

    add GivenFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,11 +3,13 @@ var TutorialToken = artifacts.require("./TutorialToken.sol");
 var ComplexStorage = artifacts.require("./ComplexStorage.sol");
 var GivenToken = artifacts.require("./GivenToken.sol");
 var Given = artifacts.require("./Given.sol");
+var GivenFactory = artifacts.require("./GivenFactory.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
   deployer.deploy(GivenToken);
+  deployer.deploy(GivenFactory);
   deployer.deploy(Given, "hello", web3.eth.accounts[0], 150);
 };
---
+++ test/givenFactory.js
@@ -0,0 +1,5 @@
+const GivenFactory = artifacts.require("./GivenFactory.sol")
+
+contract('Given Factory', (accounts) => {
+})
+ 
