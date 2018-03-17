8e52dcd4e1694ad0785b909ee41553fbbd38084c
Fan Jiang <fanjiang@thoughtworks.com>
2018-03-17 23:54:18 +0800

    rm unused sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,8 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var TutorialToken = artifacts.require("./TutorialToken.sol");
-var ComplexStorage = artifacts.require("./ComplexStorage.sol");
 var GivenToken = artifacts.require("./GivenToken.sol");
-var GivenFactory = artifacts.require("./GivenFactory.sol");
 var Given = artifacts.require("./Given.sol");
 var GivenFactory = artifacts.require("./GivenFactory.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
   deployer.deploy(GivenToken);
   deployer.deploy(GivenFactory);
   deployer.deploy(Given, "hello", web3.eth.accounts[0], 150);
